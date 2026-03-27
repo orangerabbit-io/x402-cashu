07745e1c8f7aba9abbef97d45b855f35c69cb128
Adam Lindsay <me@adam-lindsay.com>
2026-03-27 13:44:27 -0400

    feat: add public exports for all scheme components

---
+++ x402-cashu/src/index.ts
@@ -0,0 +1,37 @@
+// Shared types and constants
+export {
+  type CashuConfig,
+  type CashuExtra,
+  type ProofStore,
+  CashuErrorCode,
+  CashuPaymentError,
+  CASHU_NETWORK,
+  CASHU_CAIP_FAMILY,
+  noopProofStore,
+  validateConfig,
+} from "./shared/types.js";
+
+// Token utilities
+export { parseToken, normalizeMintUrl, sumProofs, assertHttps } from "./shared/token.js";
+
+// Verification
+export {
+  verifyPayment,
+  type VerifyResult,
+  type VerifyContext,
+  type ProofState,
+} from "./shared/verify.js";
+
+// Settlement
+export { settlePayment, type SettleResult, type SettleContext } from "./shared/settle.js";
+
+// Facilitator
+export { ExactCashuFacilitator } from "./facilitator/scheme.js";
+export { registerExactCashuFacilitator } from "./facilitator/register.js";
+
+// Server
+export { ExactCashuServer } from "./server/scheme.js";
+export { registerExactCashuServer } from "./server/register.js";
+
+// Client
+export { ExactCashuClient } from "./client/scheme.js";
